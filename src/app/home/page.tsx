0b83bda197ed87fbd0e794b280bbf14002b80cf3
dara-odebode <daraodebode@gmail.com>
2025-10-13 17:15:11 +0100

    home page done!!

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -26,11 +26,13 @@ export default function HomePage() {
   }
 
   return (
-    <main className="min-h-screen flex flex-col justify-start text-center px-9 pt-1">
+    <main className="flex flex-col text-center px-9">
 
       {/* Navbar */}
-      <nav className="w-full flex justify-between items-center px-5 py-18">
+      <nav className="w-full flex justify-between items-center px-5 py-6">
         <Link href="/" className="flex items-center space-x-3">
+
+        {/* Logo image */}
           <Image 
             src="/logo.png" 
             alt="Lark Park Theatre Logo" 
@@ -50,36 +52,37 @@ export default function HomePage() {
       </nav>
 
       {/* Page content */}
-      <h1 className="text-6xl font-bold mb-26 text-left leading-snug px-8 mt-10 tracking-wide">
+
+
+      {/* header */}
+      <h1 className="text-6xl font-bold mb-29 text-left leading-snug px-8 mt-37 tracking-wide">
         WHERE PERFORMANCE<br />MEETS PURPOSE.</h1>
         
-{/* Description section */}
-<div className="bg-[#77211F] p-8 mt-8 flex flex-row justify-between items-start">
+      {/* Description section */}
+      <div className="bg-[#77211F] p-8 mt-8 flex flex-col md:flex-row items-start justify-between h-67">
 
-  {/* Text section */}
-  <div className="flex-1 pr-8 flex flex-col justify-start">
-    <p className="max-w-4xl text-[#F3EAE5] font-light text-left leading-relaxed">
-      Lark Park Theatre specialise in bringing theatre to communities through thought-provoking, engaging, and exciting workshops. With a focus on collaborative devising, Lark Park facilitates the creation of stories most relevant to the people they work with.
-    </p>
+        {/* Text section */}
+        <div className="flex-1 pr-8 flex flex-col justify-start">
+          <p className="max-w-4xl text-[#F3EAE5] font-light text-left leading-relaxed mt-6">
+            Lark Park Theatre specialise in bringing theatre to communities through thought-provoking, engaging, and exciting workshops. With a focus on collaborative devising, Lark Park facilitates the creation of stories most relevant to the people they work with.
+          </p>
 
-    <p className="max-w-4xl text-[#F3EAE5] font-light text-left leading-relaxed mt-6">
-      As a producing company, Lark Park works with exciting emerging theatre artists, connecting them with venues and supporting the creation of bold new work.
-    </p>
-  </div>
+          <p className="max-w-4xl text-[#F3EAE5] font-light text-left leading-relaxed mt-6">
+            As a producing company, Lark Park works with exciting emerging theatre artists, connecting them with venues and supporting the creation of bold new work.
+          </p>
+        </div>
 
-  {/* Bird image */}
-  <div className="flex-shrink-0 self-start">
-    <Image 
-      src="/bird.png" 
-      alt="bird image" 
-      width={400} 
-      height={400} 
-      className="object-contain -scale-x-100"
-    />
-  </div>
-</div>
-
-
+        {/* Bird image */}
+        <div className="flex-shrink-0 self-start -mt-23 -mr-px">
+          <Image 
+            src="/bird.png" 
+            alt="bird image" 
+            width={389} 
+            height={400} 
+            className="object-contain -scale-x-100"
+          />
+        </div>
+      </div>
     </main>
   )
 }
