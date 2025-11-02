0050d77718640789f42d6adefa0cdf0d9ab1d941
dara-odebode <daraodebode@gmail.com>
2025-11-02 11:59:11 +0000

    started projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -51,49 +51,16 @@ export default function HomePage() {
         </div>
       </nav>
 
-      {/* Page content */}
 
-      {/* header */}
-      <h1 className="text-6xl font-bold mb-29 text-left leading-snug px-8 mt-37 tracking-wide">
-        WHERE PERFORMANCE <br /> MEETS PURPOSE.</h1>
-        
-      {/* Description section */}
-      <div className="bg-[#77211F] p-8 mt-8 flex flex-col md:flex-row items-start justify-between h-67">
-
-        {/* Text section */}
-        <div className="flex-1 pr-8 flex flex-col justify-start">
-          <p className="max-w-4xl text-[#F3EAE5] font-light text-left leading-relaxed mt-6">
-            Lark Park Theatre specialise in bringing theatre to communities through thought-provoking, engaging, and exciting workshops. With a focus on collaborative devising, Lark Park facilitates the creation of stories most relevant to the people they work with.
-          </p>
-
-          <p className="max-w-4xl text-[#F3EAE5] font-light text-left leading-relaxed mt-6">
-            As a producing company, Lark Park works with exciting emerging theatre artists, connecting them with venues and supporting the creation of bold new work.
-          </p>
-        </div>
-
-        {/* Bird image */}
-        <div className="flex-shrink-0 self-start -mt-23 -mr-px">
-          <Image 
-            src="/bird.png" 
-            alt="bird image" 
-            width={389} 
-            height={400} 
-            className="object-contain -scale-x-100"/>
-        </div>
-      </div>
-
-      {/*ABOUT SECTION */}
-
-      {/* Header */}
-      <div className="">
-      <p className="ml-8 text-[#C64534] font-light text-left leading-relaxed mt-45">
-        ABOUT US</p>
+      {/*  Community Projects Header */}
+     
+      <p className="ml-8 text-[#C64534] text-left leading-relaxed mt-18">
+        Previous Projects</p>
       <h1 className="text-6xl font-bold mb-15 text-left leading-snug px-8 tracking-wide">
-        WHO WE ARE</h1>
-        </div>
+        Community Projects</h1>
 
       {/* Images */}
-      <div className="columns-2 gap-50 w-240 h-auto mb-12 ml-60 text-2xl">
+      <div className="columns-2 gap-40 w-240 h-auto mb-12 ml-10 text-2xl">
         <div className="text-left space-y-2">
           <img src="/Chloe.jpg" alt="Chloe" className="float-none" />
           <p className="text-2xl font-medium">Chloe Coombes</p>
@@ -110,28 +77,22 @@ export default function HomePage() {
         </div>
       </div>
 
-      {/* Description section */}
-      <div className="bg-[#77211F] p-8 mt-8 mb-20 flex flex-col md:flex-row items-start justify-between h-67">
+      
+      {/* Production Work Header */}
+    
+      <h1 className="text-6xl font-bold mb-15 text-left leading-snug px-8 tracking-wide mt-10">
+        Production Work</h1>
 
-        {/* Bird image */}
-        <div className="flex-shrink-0 self-start -mt-23 -mr-px">
-          <Image 
-            src="/bird.png" 
-            alt="bird image" 
-            width={389} 
-            height={400} 
-            className="object-contain scale-x-100"/>
-        </div>
-        
-        {/* Text section */}
-        <div className="flex-1 pr-8 flex flex-col justify-start">
-          <p className="max-w-4xl text-[#F3EAE5] font-light text-left leading-relaxed mt-6">
-          Trained at Guildford School of Acting on BA(Hons) Applied and Contemporary Theatre. As facilitators Ashleigh-Ann and Chloe have experience working with KS3, KS4, and KS5 students, young carers and adults with additional learning needs.</p>
+      {/* Images */}
+      <div className="columns-2 gap-40 w-240 h-auto mb-12 ml-10 text-2xl">
+        <div className="text-left space-y-2">
+          <img src="/Chloe.jpg" alt="Chloe" className="float-none" />
+          <p className="text-2xl font-medium">Chloe Coombes</p>
+          <p className="italic font-normal">She/her</p>
+          <p className="text-sm font-medium">Writer, director, facilitator and performer. Debut play Tea was produced by UpYoursCo in 2024, Burnt Orange Theatre Company in 2023, and self-produced in 2023.</p>
         </div>
       </div>
 
     </main>
   )
 }
-
-  
