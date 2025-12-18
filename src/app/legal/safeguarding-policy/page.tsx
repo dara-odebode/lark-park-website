983bc744e46d47a4a6ed5e155cec4be7a8f5442b
dara-odebode <daraodebode@gmail.com>
2025-12-18 19:00:58 +0000

    corrected safeguading link

--- src/app/legal/safeguarding-policy/page.tsx
+++ src/app/legal/safeguarding-policy/page.tsx
@@ -59,7 +59,7 @@ const Navbar = () => {
           {open && (
             <div className="absolute right-0 bg-[#F3EAE5] border border-[#C64534] shadow-lg w-48">
           <Link
-            href="/legal/safeguarding"
+            href="/legal/safeguarding-policy"
             className="block px-4 py-2 text-left text-[#191919] hover:bg-[#C64534] hover:text-white"
           >
             Safeguarding Policy
