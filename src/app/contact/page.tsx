f1f2a03f0c11b32ee0ff0fd4172a0f2b5cc88a65
dara-odebode <daraodebode@gmail.com>
2025-12-19 16:36:09 +0000

    adding margin to bottom

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -120,7 +120,7 @@ return (
   </div>
 
     {/* image */}
-  <div className="hidden md:block md:w-[600px] lg:w-[720px] relative md:-mt-30 mb-70 md:mb-0">
+  <div className="hidden md:block md:w-[600px] lg:w-[720px] relative md:-mt-30">
     <Image
       src="/orange_lark.png"
       alt="Lark illustration"
@@ -130,6 +130,9 @@ return (
       priority
     />
   </div>
+
+  <div className="mb-47 md:mb-0">
+</div>
 
 </div>
     </main>
